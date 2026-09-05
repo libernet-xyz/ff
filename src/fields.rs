368d66a01e07821f5cf63b311ca1def08ff9523d
71104 <a71104@gmail.com>
2026-09-05 14:06:40 +0200

    All fields have `to_u256()` and `to_u512()`.

--- src/fields.rs
+++ src/fields.rs
@@ -370,6 +370,12 @@ pub trait Field:
 
     /// Returns this scalar as a `u16`, or `None` if the value exceeds the 16-bit range.
     fn try_to_u16(&self) -> Option<u16>;
+
+    /// Returns this scalar as a [`U256`].
+    fn to_u256(&self) -> U256;
+
+    /// Returns this scalar as a [`U512`].
+    fn to_u512(&self) -> U512;
 }
 
 /// Describes a field with a (3^T)-th root of unity.
@@ -481,12 +487,6 @@ pub trait Field256: Field + From<u32> + From<u64> + From<u128> {
 
     /// Returns this scalar as a `u128`, or `None` if the value exceeds the 128-bit range.
     fn try_to_u128(&self) -> CtOption<u128>;
-
-    /// Returns this scalar as a [`U256`].
-    fn to_u256(&self) -> U256;
-
-    /// Returns this scalar as a [`U512`].
-    fn to_u512(&self) -> U512;
 }
 
 /// A [`Field`] whose order is a prime number, ie. a field of the form `GF(p)` as opposed to an
